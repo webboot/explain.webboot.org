16d4fb5478d47e58f04afacdfcc110c20ee2d8b7
jascha <jascha@jaeh.at>
2020-02-21 01:00:38 +0100

    continue adding text

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -5,70 +5,64 @@ export const View = state => [
     h2('Abstract:'),
 
     p([
-      "satoshi's solution to the ",
+      span({ title: 'NOT Craig Wright.' }, "Satoshi Nakamoto's"),
+      ' solution to the ',
       Link({ to: 'https://en.wikipedia.org/wiki/Byzantine_fault' }, 'byzantine fault'),
-      ' problem enabled the secure and trustless transaction of digital currencies.',
+      ' problem enabled the secure and trustless transaction of digital currency tokens.',
     ]),
+
     p([
-      'The Bitcoin Protocol was simply the glue stitching together',
+      'The Bitcoin Protocol simply provided the glue stitching together',
       ' many existing technologies into a new ecosystem of usecases.',
     ]),
 
     p([
       '@webboot integrates smart contract enabled distributed hash tables with browsers',
-      ' to provide client-side version management, caching, and subresource integrity',
-      " guarantees for the code being executed on end-users' devices.",
+      ' to facilitate client-side version management, caching, and subresource integrity guarantees',
+      ' for any type of publicly addressible data.',
     ]),
 
     p([
-      "@webboot's provable verification of the integrity of any piece",
-      ' of addressible content aims to increase privacy and security guarantees',
-      ' for developers and end-users.',
+      '@webboot provides a solution to provably verify the integrity of any',
+      ' piece of addressible content,',
+      ' increasing privacy and security guarantees for developers and end-users.',
     ]),
   ]),
 
   div({ id: 'introduction' }, [
-    h2('@webboot'),
-
-    p([
-      Link({ to: 'https://webboot.org' }, '@webboot'),
-      ' aims to make ',
-      Link({ to: 'https://en.wikipedia.org/wiki/Trust_on_first_use' }, 'tofu - trust on first use'),
-      ' a bit less scary.',
-    ]),
+    h2('Introduction'),
 
     h3('webboot is:'),
 
     p([
-      'The missing source of integrity protocol for the web,',
-      ' utilizing a distributed network of validator nodes to provide both developers and users',
-      ' with a toolset to verify the integrity of any piece of publicly addressable content,',
-      ' including homepages, webapps, and dapps.',
+      'A verifiable source of integrity protocol for the web,',
+      ' utilizing a distributed network of validator nodes',
+      ' to provide both developers and users with a toolset',
+      ' to verify the integrity of any piece of publicly addressable content',
+      ' rendered or executed in homepages, webapps, and dapps.',
     ]),
 
-    h3('webboot solves:'),
+    h3('webboot provides solutions for:'),
 
     h4('Tofu - Trust on first use'),
     p([
       'By making it possible to verify any published data,',
-      'webboot solves the technological half of the tofu problem.',
+      ' webboot solves the technological half of the tofu problem.',
     ]),
 
     h4('Tosu - Trust on subsequent use'),
-    p('Webboot adds functionality to control version updates for both developers and endusers.'),
+    p('Webboot adds functionality to control version updates for both developers and end-users.'),
 
-    h3('webboot helps mitigating'),
-
-    h4('Social Engineering / Phishing attacks'),
+    h4('Phishing'),
     p([
-      'By saving your security critical homepages in @webboot,',
-      ' the browser extension can warn you if you load a url that',
-      ' looks similar to one you already saved.',
+      "Webboot can compare any loaded url to the urls saved in it's cache",
+      ' making it possible to calculate the ',
+      Link({ to: 'https://en.wikipedia.org/wiki/Levenshtein_distance' }, 'Levenshtein distance'),
+      ' and other similarity indicators of requested urls.',
     ]),
-
     p([
-      'Utilising that knowledge of the past,',
-      ' clicking on a link in a phishing email will show a webboot error.',
+      'This allows us to warn the user about a possible phishing attempt,',
+      ' before even loading the target url and checking the contents it returns.',
     ]),
   ]),
 
@@ -99,9 +93,62 @@ export const View = state => [
     ]),
   ]),
 
-  div({ id: 'problem-tofu' }, [h2('The Tofu Problem')]),
+  div({ id: 'problem-tofu' }, [
+    h2('The Tofu Problem'),
 
-  div({ id: 'problem-tosu' }, [h2('The Tosu Problem')]),
+    h4('Trust on first use'),
+
+    p('How can we trust data someone sends us did not get changed on the way?'),
+
+    p([
+      'By adding @webboot as an intermediary to any data exchange workflow,',
+      ' there suddenly is a third party in the mix,',
+      ' verifying the integrity of the exchanged data on both ends.',
+    ]),
+
+    p([
+      'Since there is publicly attestable proof of those transactions too,',
+      ' future attestation of past proofs is no issue either,',
+      ' which allows adding an additional layer',
+      'to (de)centralized ',
+      Link({ to: 'https://en.wikipedia.org/wiki/Provenance' }, 'proof of provenance'),
+      ' applications.',
+    ]),
+  ]),
+
+  div({ id: 'problem-tosu' }, [
+    h2('The Tosu Problem'),
+
+    h4('Trust on subsequent use'),
+
+    p('TOSU. we made that up.'),
+    p('Being written somewhere on the internet makes it a thing now, though.'),
+
+    p([
+      'Lets assume we solved the TOFU problem;',
+      ' we loaded the first version of a homepage in a trustable way,',
+      ' and we have the working application running on our device. w00t!',
+    ]),
+
+    p([
+      'But what happens when on second load,',
+      ' we discover that a piece of data we have loaded has changed?',
+      ' At the least, we again have to verify the data',
+      ' and its origin just as we did on the first load.',
+    ]),
+
+    p([
+      'Simply said:',
+      ' We ',
+      Link({ to: 'TODO: rfc2119#MUSTNOT' }, 'MUST NOT'),
+      ' trust the server to send us valid code,',
+      ' on the contrary, we ',
+      Link({ to: 'TODO: rfc2119#SHOULD' }, 'SHOULD'),
+      ' assume to receive malicious payloads on every load.',
+    ]),
+
+    p('TODO: add paragraph explaining user updating'),
+  ]),
 
   div({ id: 'problem-threat' }, [h2('Threat Vectors')]),
 ]
